7f7848a7d3764b3bc794b6ab562aa7dbf26c6560
SamuelBhoop <sabuca6@gmail.com>
2026-05-25 10:03:37 -0500

    un cambio bobito

--- src/components/DragonList.tsx
+++ src/components/DragonList.tsx
@@ -2,7 +2,7 @@ import DragonCard from './DragonCard'
 
 export default function DragonList({ dragons }: { dragons: any[] }) {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
       {dragons.map((dragon) => (
         <DragonCard key={dragon.name} dragon={dragon} />
       ))}
